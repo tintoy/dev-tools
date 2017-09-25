c16622c099330000bcae0a84fc7f44de782fe72a
Adam Friedman <tintoy@users.noreply.github.com>
2017-09-26 09:18:27 +1000

    Remove JDK7, other minor nits.

--- windows/scripts/DevTools.ps1
+++ windows/scripts/DevTools.ps1
@@ -1,24 +1,25 @@
 Param(
     [switch] $DotNet,
     [switch] $CPP,
-    [switch] $Jdk7,
     [switch] $Jdk8,
     [switch] $Python2,
-    [switch] $Python3
+    [switch] $Python3,
+    [switch] $Ruby22
 )
 
 # Constants
 $DevToolsDirectory = 'D:\Development\Tools'
+$VisualStudioDirectory = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Professional"
 
 ##################
 # Helper functions
 
 Function Get-VsCommonToolsDirectory
 {
-    $vsBaseDirectory = Get-32BitSoftwareRegistryValue -RelativePath 'Microsoft\VisualStudio\SxS\VS7' -Name '14.0'
+    $vsBaseDirectory = Get-32BitSoftwareRegistryValue -RelativePath 'Microsoft\VisualStudio\SxS\VS7' -Name '15.0'
     If (!$vsBaseDirectory)
     {
-        Throw 'Cannot find Visual Studio 2015 base directory in the registry.';
+        Throw 'Cannot find Visual Studio 2017 base directory in the registry.';
     }
 
     $vsCommonToolsDirectory = Join-Path $vsBaseDirectory 'Common7\Tools'
@@ -76,14 +77,11 @@ Function Convert-EnvironmentStringToPathList([string] $PathString) {
 }
 
 Function Convert-PathsToEnvironmentString([string[]] $PathSet) {
-    Return [string]::Join(";", $PathSet)
+    Return [string]::Join(";", (Select -InputObject $PathSet -Unique))
 }
 
-If ($Jdk7) {
-    $JdkVersion = '1.7.0_79'
-}
-ElseIf ($Jdk8) {
-    $JdkVersion = '1.8.0_60'
+If ($Jdk8) {
+    $JdkVersion = '1.8.0_112'
 }
 
 #######################
@@ -95,8 +93,8 @@ If ($DotNet) {
     $env:FrameworkDIR32 = 'C:\WINDOWS\Microsoft.NET\Framework\'
     $env:FrameworkVersion = 'v4.0.30319'
     $env:FrameworkVersion32 = 'v4.0.30319'
-    $env:NETFXSDKDir = 'C:\Program Files (x86)\Windows Kits\NETFXSDK\4.6\'
-    $env:FSHARPINSTALLDIR = Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\F#\4.0\Framework\v4.0\'
+    $env:NETFXSDKDir = 'C:\Program Files (x86)\Windows Kits\NETFXSDK\4.6/1\'
+    $env:FSHARPINSTALLDIR = Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\F#\4.1\Framework\v4.0\'
 }
 
 If ($CPP) {
@@ -104,25 +102,25 @@ If ($CPP) {
     $includes = Convert-EnvironmentStringToPathList($env:INCLUDE)
 
     $includes.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\VC\INCLUDE')
+        (Join-Path $VisualStudioDirectory 'VC\Tools\MSVC\14.11.25503\INCLUDE')
     ) | Out-Null
     $includes.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\VC\ATLMFC\INCLUDE')
+        (Join-Path $VisualStudioDirectory 'VC\Tools\MSVC\14.11.25503\ATLMFC\INCLUDE')
     ) | Out-Null
     $includes.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\include\10.0.14393.0\ucrt')
+        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\include\10.0.15063.0\ucrt')
     ) | Out-Null
     $includes.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\NETFXSDK\4.6\include\um')
+        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\NETFXSDK\4.6.1\include\um')
     ) | Out-Null
     $includes.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\include\10.0.14393.0\shared')
+        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\include\10.0.15063.0\shared')
     ) | Out-Null
     $includes.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\include\10.0.14393.0\um')
+        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\include\10.0.15063.0\um')
     ) | Out-Null
     $includes.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\include\10.0.14393.0\winrt')
+        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\include\10.0.15063.0\winrt')
     ) | Out-Null
 
     $env:INCLUDE = Convert-PathsToEnvironmentString $includes
@@ -131,19 +129,19 @@ If ($CPP) {
     $libraries = Convert-EnvironmentStringToPathList $env:LIB
 
     $libraries.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\VC\LIB')
+        (Join-Path $VisualStudioDirectory 'VC\Tools\MSVC\14.11.25503\LIB')
     ) | Out-Null
     $libraries.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\VC\ATLMFC\LIB')
+        (Join-Path $VisualStudioDirectory 'VC\Tools\MSVC\14.11.25503\ATLMFC\LIB')
     ) | Out-Null
     $libraries.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\lib\10.0.14393.0\ucrt\x86')
+        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\lib\10.0.15063.0\ucrt\x86')
     ) | Out-Null
     $libraries.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\NETFXSDK\4.6\lib\um\x86')
+        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\NETFXSDK\4.6.1\lib\um\x86')
     ) | Out-Null
     $libraries.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\lib\10.0.14393.0\um\x86')
+        (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\lib\10.0.15063.0\um\x86')
     ) | Out-Null
 
     $env:LIB = Convert-PathsToEnvironmentString $libraries
@@ -159,10 +157,10 @@ If ($DotNet) {
 }
 If ($CPP) {
     $libraryPaths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\VC\LIB')
+        (Join-Path $VisualStudioDirectory 'VC\Tools\MSVC\14.11.25503\LIB')
     ) | Out-Null
     $libraryPaths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\VC\ATLMFC\LIB')
+        (Join-Path $VisualStudioDirectory 'VC\Tools\MSVC\14.11.25503\ATLMFC\LIB')
     ) | Out-Null
     $libraryPaths.Add(
         (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\UnionMetadata')
@@ -191,56 +189,41 @@ $paths = Convert-EnvironmentStringToPathList $env:Path
 
 If ($DotNet) {
     $paths.Add(
-        (Join-Path ${env:UserProfile} '.dnx\bin')
+        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\F#\4.1\Framework\v4.0')
     ) | Out-Null
     $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'HTML Help Workshop')
+        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\TypeScript\2.1')
     ) | Out-Null
     $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\F#\4.0\Framework\v4.0')
+        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools')
     ) | Out-Null
     $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\TypeScript\1.0')
+        (Join-Path $VisualStudioDirectory 'Common7\IDE')
     ) | Out-Null
     $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\TypeScript\1.5')
+        (Join-Path $VisualStudioDirectory 'Common7\IDE\CommonExtensions\Microsoft\TestWindow')
     ) | Out-Null
     $paths.Add(
-        (Join-Path ${env:windir} 'Microsoft.NET\Framework\v4.0.30319')
+        (Join-Path $VisualStudioDirectory 'Common7\Tools')
     ) | Out-Null
     $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools')
+        (Join-Path $VisualStudioDirectory 'Team Tools\Performance Tools')
     ) | Out-Null
     $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\Common7\IDE')
+        (Join-Path $DevToolsDirectory 'DocFX')
     ) | Out-Null
+}
+If ($CPP) {
     $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow')
-    ) | Out-Null
-    $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\Common7\Tools')
-    ) | Out-Null
-    $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\Team Tools\Performance Tools')
+        (Join-Path $VisualStudioDirectory 'VC\Tools\MSVC\14.11.25503\bin\HostX64\x64')
     ) | Out-Null
     $paths.Add(
         (Join-Path ${env:ProgramFiles} 'CMake\bin')
     ) | Out-Null
 }
-If ($CPP) {
-    $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\VC\BIN')
-    ) | Out-Null
-    $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\VC\VCPackages')
-    ) | Out-Null
-    $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'CMake\bin')
-    ) | Out-Null
-}
 If ($DotNet -Or $CPP) {
     $paths.Add(
-        (Join-Path ${env:ProgramFiles(x86)} 'MSBuild\14.0\bin')
+        (Join-Path $VisualStudioDirectory 'MSBuild\15.0\bin')
     ) | Out-Null
     $paths.Add(
         (Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\bin\x86')
@@ -248,7 +231,7 @@ If ($DotNet -Or $CPP) {
 }
 If ($DotNet) {
     $paths.Add(
-        (Join-Path $env:windir 'C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319')
+        (Join-Path $env:windir 'Microsoft.NET\Framework\v4.0.30319')
     ) | Out-Null
 }
 If ($JdkVersion) {
@@ -264,7 +247,10 @@ If ($JdkVersion) {
         "$DevToolsDirectory\gradle\bin"
     ) | Out-Null
     $paths.Add(
-        "$DevToolsDirectory\activator"
+        "$DevToolsDirectory\sbt\bin"
+    ) | Out-Null
+    $paths.Add(
+        (Join-Path $DevToolsDirectory 'activator\bin')
     ) | Out-Null
     $paths.Add(
         (Join-Path $env:USERPROFILE 'bin') # Local binaries (required for stuff that expects unix-style ~/bin)
@@ -280,10 +266,15 @@ If ($Python2) {
 }
 If ($Python3) {
     $paths.Add(
-        "$DevToolsDirectory\Python\v35"
+        "$DevToolsDirectory\Python\v36"
     ) | Out-Null
     $paths.Add(
-        "$DevToolsDirectory\Python\v35\Scripts"
+        "$DevToolsDirectory\Python\v36\Scripts"
+    ) | Out-Null
+}
+If ($Ruby22) {
+    $paths.Add(
+        "$DevToolsDirectory\Ruby\DevKit\bin"
     ) | Out-Null
 }
 
@@ -294,21 +285,29 @@ $env:UCRTVersion = '10.0.1 0240.0'
 $env:UniversalCRTSdkDir = Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\'
 
 If ($CPP) {
-    $env:VCINSTALLDIR = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\VC\'
+    $env:VCINSTALLDIR = Join-Path $VisualStudioDirectory 'VC\Tools\MSVC\14.11.25503\'
     $env:WindowsLibPath = 'C:\Program Files (x86)\Windows Kits\10\UnionMetadata;C:\Program Files (x86)\Windows Kits\10\References;C:\Program Files (x86)\Windows Kits\10\References\Windows.Foundation.UniversalApiContract\1.0.0.0;C:\Program Files (x86)\Windows Kits\10\References\Windows.Foundation.FoundationContract\1.0.0.0;C:\Program Files (x86)\Windows Kits\10\References\indows.Networking.Connectivity.WwanContract\1.0.0.0'
+}
+
+If ($Python3) {
+    $env:CLOUDSDK_PYTHON='D:\Development\Tools\Python\v27\bin\python.exe'
+}
+
+If ($Ruby22) {
+    $env:RI_DEVKIT = "$DevToolsDirectory\Ruby\DevKit"
 }
 
 $env:VisualStudioVersion = '14.0'
 $env:VS110COMNTOOLS = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 11.0\Common7\Tools\'
 $env:VS120COMNTOOLS = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 12.0\Common7\Tools\'
-$env:VS140COMNTOOLS = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\Common7\Tools\'
-$env:VSINSTALLDIR = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\'
-$env:VSSDK140Install = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio 14.0\VSSDK\'
+$env:VS140COMNTOOLS = Join-Path $VisualStudioDirectory 'Common7\Tools\'
+$env:VSINSTALLDIR = Join-Path $VisualStudioDirectory ''
+$env:VSSDK140Install = Join-Path $VisualStudioDirectory 'VSSDK\'
 $env:WindowsSdkDir = Join-Path ${env:ProgramFiles(x86)} 'Windows Kits\10\'
-$env:WindowsSDKLibVersion = '10.0.14393.0\'
-$env:WindowsSDKVersion = '10.0.14393.0\'
-$env:WindowsSDK_ExecutablePath_x64 = Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\x64\'
-$env:WindowsSDK_ExecutablePath_x86 = Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\'
+$env:WindowsSDKLibVersion = '10.0.15063.0\'
+$env:WindowsSDKVersion = '10.0.15063.0\'
+$env:WindowsSDK_ExecutablePath_x64 = Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\x64\'
+$env:WindowsSDK_ExecutablePath_x86 = Join-Path ${env:ProgramFiles(x86)} 'Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\'
 
 # Helper functions
 If ($DotNet) {
@@ -323,9 +322,6 @@ If ($DotNet) {
 }
 If ($CPP) {
     $devComponents += 'C++'
-}
-If ($Jdk7) {
-    $devComponents += 'JDK7'
 }
 If ($Jdk8) {
     $devComponents += 'JDK8'
